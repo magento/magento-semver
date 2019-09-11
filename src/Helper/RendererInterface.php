ea60d91b37a0ae2414f1a9cba97b1a328adf2261
Tom Erskine <terskine@adobe.com>
2019-09-11 11:10:18 -0500

    EQPS-213 : Reimplemented framework Phrase, PlaceHholder and RendererInterface within magento-semver

---
+++ src/Helper/RendererInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: terskine
+ * Date: 9/11/19
+ * Time: 10:31 AM
+ */
+
+namespace Magento\SemanticVersionChecker\Helper;
+
+
+class RendererInterface
+{
+
+}
