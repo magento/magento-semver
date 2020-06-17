32a63121f65c1581f97f6bc171ff853d2a3eaf4f
Maksym Aposov <xmav@users.noreply.github.com>
2020-06-17 10:01:29 -0500

    MC-33156: Update magento-semver to be compatible with PHP7.4

--- src/Helper/ClassParser.php
+++ src/Helper/ClassParser.php
@@ -96,7 +96,7 @@ class ClassParser
         }
 
         foreach ($nodeTree->stmts as $stmt) {
-            if ($stmt instanceof Use_ && $stmt->uses[0]->alias === $extendedClass) {
+            if ($stmt instanceof Use_ && $stmt->uses[0]->getAlias()->toString() === $extendedClass) {
                 return implode("\\", $stmt->uses[0]->name->parts);
             }
         }
@@ -262,7 +262,7 @@ class ClassParser
         $namespace = $nodeTree->name->toString();
         foreach ($nodeTree->stmts as $stmt) {
             if ($stmt instanceof Use_) {
-                $uses[$stmt->uses[0]->alias] = $stmt->uses[0];
+                $uses[$stmt->uses[0]->getAlias()->toString()] = $stmt->uses[0];
             }
         }
 
@@ -310,9 +310,9 @@ class ClassParser
                 //is the class, interface, trait defined in the very same file?
                 if (
                     $stmt instanceof ClassLike
-                    && $stmt->name === $alias
+                    && $stmt->name->toString() === $alias
                 ) {
-                    return $nodeTree->name->toString() . '\\' . $stmt->name;
+                    return $nodeTree->name->toString() . '\\' . $stmt->name->toString();
                 }
 
                 //is the class being imported?
@@ -320,7 +320,7 @@ class ClassParser
                     foreach ($stmt->uses as $useUseStmnt) {
                         $fullyQualifiedName = $useUseStmnt->name->toString();
 
-                        if ($useUseStmnt->alias === $alias || $fullyQualifiedName === $alias) {
+                        if ($useUseStmnt->getAlias()->toString() === $alias || $fullyQualifiedName === $alias) {
                             return $fullyQualifiedName;
                         }
                     }
