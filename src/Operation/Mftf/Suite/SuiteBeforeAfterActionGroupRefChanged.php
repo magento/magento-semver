f39d9c4a3eaf8ff086f01fe0bcd3e3b238c14232
Soumya Unnikrishnan <sunnikri@adobe.com>
2020-11-04 11:37:34 -0600

    MQE-2354: [MFTF SVC] Change "<test> ref change" from MAJOR  to a MINOR

--- src/Operation/Mftf/Suite/SuiteBeforeAfterActionGroupRefChanged.php
+++ src/Operation/Mftf/Suite/SuiteBeforeAfterActionGroupRefChanged.php
@@ -21,7 +21,7 @@ class SuiteBeforeAfterActionGroupRefChanged extends MftfOperation
      * Operation Severity
      * @var int
      */
-    protected $level = Level::MAJOR;
+    protected $level = Level::MINOR;
 
     /**
      * Operation message.
